c772f684040c87e46500f131c0c27de77d39bdd2
psiana011 <psiana011.endo@gmail.com>
2024-12-11 03:42:16 +0900

    Upd: Nix flake config file を追加

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "python with uv";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ ];
+
+          BuildInputs = [ ];
+
+          packages = [
+            pkgs.python312
+            pkgs.uv
+            pkgs.ruff
+          ];
+
+          # numpy 依存ライブラリへの PATH
+          # 他にも uv add <パッケージ名> をした後に実行してランタイムエラーになる場合, 
+          # ここにライブラリを書けば解決するかもしれない
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+            libgcc.lib
+            libz
+          ]);
+
+          shellHook = ''
+            source .venv/bin/activate
+          '';
+        };
+      });
+}
