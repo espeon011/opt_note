3b227fe830ad0df6ff0784ce4ad1084e4b7162aa
psiana011 <psiana011.endo@gmail.com>
2025-09-11 00:11:21 +0900

    fix

--- src/scsp/model_wmm_hexaly.py
+++ src/scsp/model_wmm_hexaly.py
@@ -41,8 +41,8 @@ def _(mo):
 
     **決定変数**
 
-    - $w_{ij} \in \mathbb{N}$: 文字列 $s_i$ の $j$ 文字目の重み. $(i \in \{ 1, \dots, n \}, \ j \in \{ 1, \dots, |s_i| \})$
-        - $w_i = \{ w_{i,1} \dots, w_{i,|s_i|} \}$ とおく. 
+    - $w_{ij} \in \mathbb{N}$: 文字列 $s_i$ の $j$ 文字目の重み. $(i \in \lbrace 1, \dots, n \rbrace, \ j \in \lbrace 1, \dots, |s_i| \rbrace)$
+        - $w_i = \lbrace w_{i,1} \dots, w_{i,|s_i|} \rbrace$ とおく. 
 
 
     **目的関数**
@@ -52,7 +52,7 @@ def _(mo):
     - 解 $\mathrm{sol}$ を空文字列で初期化する. 
     - 各文字 $c$ に対して重み $\sum_{i=1, \ s_i[0] = c}^n w_{i,1}$ を計算し, 重みが最大である $c$ を求める.
     - $\mathrm{sol}$ の後ろに $c$ を追加する. 
-    - 各文字列 $s_i \ (i \in \{ 1, \dots, n \})$ に対し, 先頭の文字が $c$ である場合は
+    - 各文字列 $s_i \ (i \in \lbrace 1, \dots, n \rbrace)$ に対し, 先頭の文字が $c$ である場合は
         - $s_i$ の先頭の文字を削除する.
         - $w_i$ の先頭の重みを削除し, インデックスを前に詰める. 
     - $s_1, \dots, s_n$ 全てが空文字列になれば終了. $\mathrm{sol}$ が解.
