02dc306600d110ea1a146b61f34394f3481fdb4a
psiana011 <psiana011.endo@gmail.com>
2025-12-01 03:22:41 +0900

    example モジュールの説明を修正

--- src/opt_note/scsp/example/__init__.py
+++ src/opt_note/scsp/example/__init__.py
@@ -6,8 +6,6 @@ from typing import Literal
 import pathlib
 from .. import util
 
-DATA_DIR = pathlib.Path(__file__).parent
-
 ExampleFileName = Literal[
     "nucleotide_n005k010.txt",
     "nucleotide_n010k010.txt",
@@ -47,8 +45,8 @@ ExampleFileName = Literal[
 def load(filename: ExampleFileName) -> list[str]:
     """
     サンプルデータセット読み込み.
-    サンプルファイル名は https://github.com/espeon011/opt_note/tree/main/src/opt_note/scsp/example を参照.
     """
 
-    filepath = DATA_DIR / filename
+    data_dir = pathlib.Path(__file__).parent
+    filepath = data_dir / filename
     return util.parse(filepath)
